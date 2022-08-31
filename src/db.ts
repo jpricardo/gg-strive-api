457a36a71a10b4ec8e7b04d7217e08fe84a7a965
jpricardo <joaopds.ricardo@gmail.com>
2022-08-31 18:01:15 -0300

    Desabilitando buffer do db

--- src/db.ts
+++ src/db.ts
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import mongoose, { mongo } from 'mongoose';
 
 import dotenv from 'dotenv';
 dotenv.config();
@@ -7,6 +7,9 @@ const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 
 const dbUri = `mongodb+srv://${username}:${password}@cluster0.leiy1eq.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.set('bufferCommands', false);
+
 mongoose
 	.connect(dbUri)
 	.then(() => console.log('[DB] Connection established'))
