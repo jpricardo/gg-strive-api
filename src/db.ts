c213531ea688e390abbd6ca7bdba4c9a48f191c3
jpricardo <joaopds.ricardo@gmail.com>
2022-08-31 17:41:06 -0300

    Erro handling

--- src/db.ts
+++ src/db.ts
@@ -7,6 +7,14 @@ const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 
 const dbUri = `mongodb+srv://${username}:${password}@cluster0.leiy1eq.mongodb.net/?retryWrites=true&w=majority`;
-mongoose.connect(dbUri, () => console.log('Mongoose conectado!'));
+mongoose
+	.connect(dbUri)
+	.then(() => console.log('[DB] Connection established'))
+	.catch((error) => console.error(error));
+
+mongoose.connection.on('error', (error) => {
+	console.error('* Error after connection!');
+	console.error(error);
+});
 
 export default mongoose;
