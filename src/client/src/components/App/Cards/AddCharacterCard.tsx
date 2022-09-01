2c0e4f209b9d3879f132645b1c355d7ba6a3f45c
jpricardo <joaopds.ricardo@gmail.com>
2022-09-01 16:04:45 -0300

    Style changes

--- src/client/src/components/App/Cards/AddCharacterCard.tsx
+++ src/client/src/components/App/Cards/AddCharacterCard.tsx
@@ -1,4 +1,4 @@
-import { Card } from 'react-bootstrap';
+import { Card, Col, Row } from 'react-bootstrap';
 
 import styles from './index.module.css';
 
@@ -11,7 +11,13 @@ const AddCharacterCard: React.FC<Props> = ({ onClick }) => {
 	return (
 		<>
 			<Card className={styles.card + ' shadow'} onClick={handleClick}>
-				<Card.Body className='p-2'>+ Character</Card.Body>
+				<Card.Body className='p-2'>
+					<Row className='h-100 justify-content-center'>
+						<Col className='text-center m-auto'>
+							<span className={styles.addText}>+ Character</span>
+						</Col>
+					</Row>
+				</Card.Body>
 			</Card>
 		</>
 	);
