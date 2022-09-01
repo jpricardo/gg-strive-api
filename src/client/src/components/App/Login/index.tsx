0bd2af10f96734db467eb66238e2458b21da0911
jpricardo <joaopds.ricardo@gmail.com>
2022-09-01 09:27:13 -0300

    Login page styles

--- src/client/src/components/App/Login/index.tsx
+++ src/client/src/components/App/Login/index.tsx
@@ -1,5 +1,5 @@
 import { FormEventHandler, useContext } from 'react';
-import { Button, Card, Col, Form, Row } from 'react-bootstrap';
+import { Button, Card, Col, FloatingLabel, Form, Row } from 'react-bootstrap';
 import { AuthContext } from '../../../store/auth-context';
 
 import styles from './index.module.css';
@@ -12,33 +12,34 @@ const Login = () => {
 	};
 
 	return (
-		<Card className={styles.card + ' px-3 py-2'}>
+		<Card className={styles.card + ' px-3 py-2 mt-4 mx-auto'}>
 			<Card.Body as='section' className='px-4 py-2'>
 				<Row as='article'>
 					<header className='text-center pt-3'>
 						<Row>
-							<h1>Bem vind@!</h1>
-						</Row>
-						<Row>
-							<h2>Faça Login para continuar...</h2>
+							<h1>Welcome Back!</h1>
 						</Row>
 					</header>
 					<Form onSubmit={handleSubmit} className='pb-2'>
-						<Form.Group as={Row}>
-							<Form.Label>Usuário</Form.Label>
-							<Form.Control type='text' required />
+						<Form.Group className='my-2'>
+							<FloatingLabel label='Username'>
+								<Form.Control type='text' required />
+							</FloatingLabel>
 						</Form.Group>
-						<Form.Group as={Row}>
-							<Form.Label>Senha</Form.Label>
-							<Form.Control type='password' required />
+						<Form.Group className='my-2'>
+							<FloatingLabel label='Password'>
+								<Form.Control type='password' required />
+							</FloatingLabel>
 						</Form.Group>
 						<Row className='mt-3'>
-							<a href='#'>Esqueci minha senha!</a>
+							<a href='#'>Forgot my password!</a>
 						</Row>
-						<Row className='mt-4 justify-content-center'>
-							<Button type='submit' className='w-100'>
-								Entrar
-							</Button>
+						<Row className='my-2 justify-content-center'>
+							<Col>
+								<Button type='submit' className='w-100'>
+									LET'S ROCK!
+								</Button>
+							</Col>
 						</Row>
 					</Form>
 				</Row>
