d52dabb322b1e2f16c39a6f2e109bdebf30a476d
jpricardo <joaopds.ricardo@gmail.com>
2022-10-09 15:59:41 -0300

    Better tests, implemented 'DoNotInstantiate' logic

--- src/entities/character.test.ts
+++ src/entities/character.test.ts
@@ -1,6 +1,7 @@
-import { expect, test } from 'vitest';
+import { describe, expect, it } from 'vitest';
+import DoNotInstantiateError from '../errors/do-not-instantiate-error';
+import InvalidPropertyError from '../errors/invalid-property-error';
 import Character from './character';
-import InvalidPropertyError from './errors/invalid-property-error';
 import { BattleType, CharacterProps } from './interfaces/character-props.js';
 
 const correctName = 'john';
@@ -9,51 +10,69 @@ const correctBattleType = BattleType.Balance;
 const correctEasyToUse = 5;
 const emptyMoves: CharacterProps['moves'] = [];
 
-const incorrectEasyToUse = 10;
-const incorrectBattleType = 'Broken';
+const invalidEasyToUse = 10;
+const invalidBattleType = 'Broken';
 
-test('Create a character', () => {
-	const props = {
-		name: correctName,
-		displayName: correctDisplayName,
-		battleType: correctBattleType,
-		easyToUse: correctEasyToUse,
-		moves: emptyMoves,
-	};
+const validProps = {
+	name: correctName,
+	displayName: correctDisplayName,
+	battleType: correctBattleType,
+	easyToUse: correctEasyToUse,
+	moves: emptyMoves,
+};
 
-	const character = Character.create(props);
+describe('Valid props', () => {
+	it('Creates instance with .create(props)', () => {
+		const props = { ...validProps };
 
-	expect(character).toBeInstanceOf(Character);
-	expect(character.name).toEqual(correctName);
-	expect(character.displayName).toEqual(correctDisplayName);
+		const instance = Character.create(props);
+
+		expect(instance).toBeInstanceOf(Character);
+		expect(instance.name).toEqual(correctName);
+		expect(instance.displayName).toEqual(correctDisplayName);
+	});
+
+	it('Creates instance with new keyword', () => {
+		const props = { ...validProps };
+
+		// @ts-ignore
+		expect(() => new Character(props)).toThrowError(DoNotInstantiateError);
+	});
+
+	it('Gets JSON', () => {
+		const props = { ...validProps };
+
+		const instance = Character.create(props);
+
+		expect(instance.toJson()).toEqual(props);
+	});
+
+	it('Saves', () => {
+		const props = { ...validProps };
+
+		const instance = Character.create(props);
+
+		expect(() => instance.save()).not.toThrowError();
+	});
+
+	it.todo('Updates');
+
+	it.todo('Deletes');
 });
 
-test('Create character with invalid easyToUse', () => {
-	const props = {
-		name: correctName,
-		displayName: correctDisplayName,
-		battleType: correctBattleType,
-		easyToUse: incorrectEasyToUse,
-		moves: emptyMoves,
-	};
+describe('Invalid props', () => {
+	it('Creates Character with invalid easyToUse', () => {
+		const props = { ...validProps, easyToUse: invalidEasyToUse };
 
-	expect(() => {
+		expect(() => Character.create(props)).toThrowError(InvalidPropertyError);
+	});
+
+	it('Creates Character with invalid battleType', () => {
+		const props = { ...validProps, battleType: invalidBattleType };
+
 		// @ts-ignore
-		Character.create(props);
-	}).toThrowError(InvalidPropertyError);
-});
+		expect(() => Character.create(props)).toThrowError(InvalidPropertyError);
+	});
 
-test('Create character with invalid battleType', () => {
-	const props = {
-		name: correctName,
-		displayName: correctDisplayName,
-		battleType: incorrectBattleType,
-		easyToUse: correctEasyToUse,
-		moves: emptyMoves,
-	};
-
-	expect(() => {
-		// @ts-ignore
-		Character.create(props);
-	}).toThrowError(InvalidPropertyError);
+	it.todo('Creates Character with invalid moves');
 });
--- src/entities/character.ts
+++ src/entities/character.ts
@@ -1,13 +1,17 @@
-import InvalidPropertyError from './errors/invalid-property-error';
-import CharacterProps, { BattleType } from './interfaces/character-props';
+import DoNotInstantiateError from '../errors/do-not-instantiate-error';
+import InvalidPropertyError from '../errors/invalid-property-error';
+import { BattleType, CharacterProps } from './interfaces/character-props';
 
 class Character {
 	private static MaxEasyToUse = 5;
 	private static MinEasyToUse = 1;
 
+	private static initializing = false;
+
 	private props: CharacterProps;
 
 	static create(props: CharacterProps) {
+		Character.initializing = true;
 		return new Character(props);
 	}
 
@@ -20,6 +24,9 @@ class Character {
 	}
 
 	private constructor(props: CharacterProps) {
+		if (!Character.initializing) throw new DoNotInstantiateError();
+
+		Character.initializing = false;
 		this.validateProps(props);
 		this.props = props;
 	}
